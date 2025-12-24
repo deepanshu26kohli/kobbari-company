9c502b72c58801796185125e20e32ec352198330
deepanshutwotechiesremote <deepanshu.twotechiesremote@gmail.com>
2025-12-24 19:33:16 +0530

    supabase error handled

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://uretrizkgtvadhqypgmw.supabase.co"
+const supabaseAnonKey = "yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyZXRyaXprZ3R2YWRocXlwZ213Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1NzY0NDYsImV4cCI6MjA4MjE1MjQ0Nn0.ZcmV0YI_WTjMJhnR0n_shI2uaKHhWAw5iMPacWW3zxU"
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
